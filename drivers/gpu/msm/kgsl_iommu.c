87620278e4e5020a3142ff10ef0137f7cac784b3
Steve Kondik <shade@chemlab.org>
2016-04-27 13:49:45 +0300

    iommu: msm: Sync with CAF state [squashed]
     * Now the IOMMU driver is in a newest state
       for MSM8974.
    
    These commits were merged into 'maximus':
    -iommu: msm: Add standalone support for secure SMMU
    -iommu: msm: Add cache maintenance for calls into TZ
    -iommu: msm: don't rely on structure format coming from TZ
    -iommu: msm: Only flush the exact list size
    -iommu: msm: Refactor printing of page fault
    -iommu: msm: Add support for LPAE bfb settings
    -iommu: msm: Refactor page table code
    -iommu: msm: Handle unmapping of PTE properly
    -iommu: msm: Add printing of 64 bits of TTBR0/1
    -iommu: msm: Refactor remote spinlock defconfig
    -iommu: msm: Add support for selecting remote spinlock in device tree
    -iommu: msm: Refactor use of remote spinlock
    -iommu: msm: Add call to set maximum mapped size
    -iommu: msm: Create secure page tables for SMMUv2 also
    -iommu: msm: Only remove bus vote on error
    -iommu: msm: Add base physical address to drvdata structure
    -iommu: msm: Add reference counting to IOMMUv1
    -iommu: msm: Program all M2V tables at once
    -iommu: msm: Move location of local iommu halt
    -arm/dt: iommu: Match latest IOMMU changes
    -defconfig: Use secured IOMMUs
    -iommu: msm: Fix incorrect tlb_invalidate on the map calls
    
    Signed-off-by: The Linux Foundation. All rights reserved.
    Signed-off-by: Steve Kondik <shade@chemlab.org>
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -76,16 +76,18 @@ static int kgsl_iommu_default_setstate(struct kgsl_mmu *mmu,
 static phys_addr_t
 kgsl_iommu_get_current_ptbase(struct kgsl_mmu *mmu);
 
-static void _iommu_lock(void)
+static void _iommu_lock(struct kgsl_iommu const *iommu)
 {
 	if (iommu_access_ops && iommu_access_ops->iommu_lock_acquire)
-		iommu_access_ops->iommu_lock_acquire();
+		iommu_access_ops->iommu_lock_acquire(
+						iommu->sync_lock_initialized);
 }
 
-static void _iommu_unlock(void)
+static void _iommu_unlock(struct kgsl_iommu const *iommu)
 {
 	if (iommu_access_ops && iommu_access_ops->iommu_lock_release)
-		iommu_access_ops->iommu_lock_release();
+		iommu_access_ops->iommu_lock_release(
+						iommu->sync_lock_initialized);
 }
 
 struct remote_iommu_petersons_spinlock kgsl_iommu_sync_lock_vars;
@@ -912,7 +914,6 @@ static int kgsl_iommu_start_sync_lock(struct kgsl_mmu *mmu)
 }
 
 #ifdef CONFIG_MSM_IOMMU_GPU_SYNC
-
 /*
  * kgsl_get_sync_lock - Init Sync Lock between GPU and CPU
  * @mmu - Pointer to mmu device
@@ -969,6 +970,12 @@ static int kgsl_iommu_init_sync_lock(struct kgsl_mmu *mmu)
 
 	return status;
 }
+#else
+static int kgsl_iommu_init_sync_lock(struct kgsl_mmu *mmu)
+{
+	return 0;
+}
+#endif
 
 /*
  * kgsl_iommu_sync_lock - Acquire Sync Lock between GPU and CPU
@@ -1662,7 +1669,7 @@ static int kgsl_iommu_start(struct kgsl_mmu *mmu)
 	 * changing pagetables we can use this lsb value of the pagetable w/o
 	 * having to read it again
 	 */
-	_iommu_lock();
+	_iommu_lock(iommu);
 	for (i = 0; i < iommu->unit_count; i++) {
 		struct kgsl_iommu_unit *iommu_unit = &iommu->iommu_units[i];
 		for (j = 0; j < iommu_unit->dev_count; j++) {
@@ -1701,7 +1708,7 @@ static int kgsl_iommu_start(struct kgsl_mmu *mmu)
 		}
 	}
 	kgsl_iommu_lock_rb_in_tlb(mmu);
-	_iommu_unlock();
+	_iommu_unlock(iommu);
 
 	/* For complete CFF */
 	kgsl_cffdump_setmem(mmu->device, mmu->setstate_memory.gpuaddr +
@@ -1846,7 +1853,6 @@ void kgsl_iommu_pagefault_resume(struct kgsl_mmu *mmu)
 						&iommu->iommu_units[i];
 			for (j = 0; j < iommu_unit->dev_count; j++) {
 				if (iommu_unit->dev[j].fault) {
-					kgsl_iommu_enable_clk(mmu, j);
 					_iommu_lock(iommu);
 					KGSL_IOMMU_SET_CTX_REG(iommu,
 						iommu_unit,
@@ -1856,7 +1862,6 @@ void kgsl_iommu_pagefault_resume(struct kgsl_mmu *mmu)
 						iommu_unit,
 						iommu_unit->dev[j].ctx_id,
 						FSR, 0);
-					kgsl_iommu_disable_clk(mmu, j);
 					_iommu_unlock(iommu);
 					iommu_unit->dev[j].fault = 0;
 				}
@@ -1978,7 +1983,7 @@ static int kgsl_iommu_default_setstate(struct kgsl_mmu *mmu,
 	}
 
 	/* Acquire GPU-CPU sync Lock here */
-	_iommu_lock();
+	_iommu_lock(iommu);
 
 	if (flags & KGSL_MMUFLAGS_PTUPDATE) {
 		if (!msm_soc_version_supports_iommu_v0()) {
@@ -2048,7 +2053,7 @@ static int kgsl_iommu_default_setstate(struct kgsl_mmu *mmu,
 	}
 unlock:
 	/* Release GPU-CPU sync Lock here */
-	_iommu_unlock();
+	_iommu_unlock(iommu);
 
 	/* Disable smmu clock */
 	kgsl_iommu_disable_clk(mmu, KGSL_IOMMU_CONTEXT_USER);
