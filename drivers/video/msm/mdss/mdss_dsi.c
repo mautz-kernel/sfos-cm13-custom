8e86131b53f833f2ddb4173254db76c858b1147d
Pranav Vashi <neobuddy89@gmail.com>
2016-05-02 17:50:50 +0300

    hammerhead: Add state notifier driver
     * This driver is a simple touchscreen state notifier
       which uses MDSS driver state to notify another drivers
       for suspend processes. It's a way more cosy than LCD notifier
       and will be used for some upstream updates.
    
    Signed-off-by: The Linux Foundation. All rights reserved.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -22,6 +22,7 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 #include <linux/lcd_notify.h>
+#include <linux/state_notifier.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -743,6 +744,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+		state_resume();
 		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
@@ -755,6 +757,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
+		state_suspend();
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
