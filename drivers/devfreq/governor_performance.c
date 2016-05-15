d5dbed6e6eec485bdab73b06814a48da52770eed
Signed-off-by: Steve Kondik <shade@chemlab.org>
2016-05-15 18:32:57 +0300

    PM / devfreq: Pull-in LA.DR.2.1.1_rb1.13 driver
     * Remove EXYNOS_BUS. Deprecated
     * Update PM_DEVFREQ system to the current state (many bugs
       fixed, new APIs added)
     * Add MSM_ADRENO_TZ governor from upstream LA.DR.2.1.1_rb1.13
     * Add Conservative governor from upstream LA.DR.2.1.1_rb1.13
     * Add Adreno Idler module for MSM_ADRENO_TZ
     * Add Simple GPU Algorithm module for MSM_ADRENO_TZ
     * Update Simple_Ondemand to the current state.
    
    -Not used yet
    
    Signed-off-by: The Linux Foundation. All rights reserved.
    Signed-off-by: Steve Kondik <shade@chemlab.org>
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- drivers/devfreq/governor_performance.c
+++ drivers/devfreq/governor_performance.c
@@ -1,8 +1,9 @@
 /*
- *  linux/drivers/devfreq/governor_performance.c
+ * linux/drivers/devfreq/governor_performance.c
  *
- *  Copyright (C) 2011 Samsung Electronics
- *	MyungJoo Ham <myungjoo.ham@samsung.com>
+ * Copyright (C) 2011, Samsung Electronics
+ *		      MyungJoo Ham <myungjoo.ham@samsung.com>
+ * Copyright (C) 2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,7 +15,8 @@
 #include "governor.h"
 
 static int devfreq_performance_func(struct devfreq *df,
-				    unsigned long *freq)
+				    unsigned long *freq,
+				    u32 *flag)
 {
 	/*
 	 * target callback should be able to get floor value as
@@ -31,17 +33,32 @@ static int devfreq_performance_handler(struct devfreq *devfreq,
 				unsigned int event, void *data)
 {
 	int ret = 0;
+	unsigned long freq;
 
-	if (event == DEVFREQ_GOV_START || event == DEVFREQ_GOV_RESUME) {
-		mutex_lock(&devfreq->lock);
-		ret = update_devfreq(devfreq);
-		mutex_unlock(&devfreq->lock);
+	mutex_lock(&devfreq->lock);
+	freq = devfreq->previous_freq;
+	switch (event) {
+		case DEVFREQ_GOV_START:
+			devfreq->profile->target(devfreq->dev.parent,
+					&freq,
+					DEVFREQ_FLAG_WAKEUP_MAXFREQ);
+			/* fall through */
+		case DEVFREQ_GOV_RESUME:
+			ret = update_devfreq(devfreq);
+			break;
+		case DEVFREQ_GOV_SUSPEND:
+			devfreq->profile->target(devfreq->dev.parent,
+					&freq,
+					DEVFREQ_FLAG_WAKEUP_MAXFREQ);
+			break;
 	}
+	mutex_unlock(&devfreq->lock);
 
 	return ret;
 }
 
-static struct devfreq_governor devfreq_performance = {
+static struct devfreq_governor devfreq_performance =
+{
 	.name = "performance",
 	.get_target_freq = devfreq_performance_func,
 	.event_handler = devfreq_performance_handler,
